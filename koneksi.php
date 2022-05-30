3a8a04ada5d0dd5143a823f357c751a0e04da91e
Desyana Ajeng Windy Indriyani <76044697+Desyanaaa@users.noreply.github.com>
2022-05-30 18:40:13 +0700

    Add files via upload

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$koneksi = mysqli_connect('localhost','root','','klinik_312010096');
+$koneksi = mysqli_connect('localhost','adminklinik','312010096','klinik_312010096');
 
 if (mysqli_connect_errno()){
 
